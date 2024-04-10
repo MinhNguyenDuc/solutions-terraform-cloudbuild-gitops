f3c55c2d85abb857a3b9ce161d6dcc5ce37d2b66
Minh Nguyen <minhnd@Minhs-MacBook-Air.local>
2024-04-10 17:21:39 +0700

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "terraform-lab-418408-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="terraform-lab-418408"
